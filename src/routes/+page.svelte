c76f96fc75fbe9898d5290cd1112c5dc3476f3b7
Phumrapee Limpianchop <git@rayriffy.com>
2022-12-28 03:48:37 +0900

    chore: change docs endpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 
   <section class="py-8">
     <a
-      href="https://github.com/rayriffy/svelte-aio"
+      href="https://github.com/rayriffy/svelte-aio#readme"
       class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
     >
       Documentation
