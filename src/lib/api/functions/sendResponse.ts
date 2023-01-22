1e650961873aa46d6a33aa6c023bccdffba9a894
Phumrapee Limpianchop <git@rayriffy.com>
2023-01-22 19:56:31 +0900

    feat: cdn cache for cloudflare

--- src/lib/api/functions/sendResponse.ts
+++ src/lib/api/functions/sendResponse.ts
@@ -10,6 +10,7 @@ export const sendResponse = (
       Vary: 'Accept',
       'Content-Type': payload.contentType ?? '',
       'Cache-Control': `public, max-age=${Math.floor(payload.maxAge / 1000)}, must-revalidate`,
+      'CDN-Cache-Control': `max-age=${Math.floor(payload.maxAge / 1000)}`,
       'Content-Length': Buffer.byteLength(payload.buffer).toString(),
       'Content-Security-Policy':
         "script-src 'none'; frame-src 'none'; sandbox;",
